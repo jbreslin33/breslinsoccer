3ace959578f1c06ed5586bf0373b08a2a25fcdbe
Jim Breslin <jbreslin33@gmail.com>
2017-07-02 09:50:44 -0400

    more culling of client side work

--- client.core.js
+++ client.core.js
@@ -186,41 +186,6 @@ update: function(t)
 }, 
 
 
-/*
-    Shared between server and client.
-    In this example, `item` is always of type game_player.
-*/
-check_collision: function( item ) 
-{
-	//Left wall.
-    	if(item.pos.x <= item.pos_limits.x_min) 
-	{
-        	item.pos.x = item.pos_limits.x_min;
-    	}
-
-        //Right wall
-    	if(item.pos.x >= item.pos_limits.x_max ) 
-	{
-        	item.pos.x = item.pos_limits.x_max;
-    	}
-    
-        //Roof wall.
-    	if(item.pos.y <= item.pos_limits.y_min) 
-	{
-        	item.pos.y = item.pos_limits.y_min;
-    	}
-
-        //Floor wall
-    	if(item.pos.y >= item.pos_limits.y_max ) 
-	{
-        	item.pos.y = item.pos_limits.y_max;
-    	}
-
-        //Fixed point helps be more deterministic
-    	item.pos.x = item.pos.x.fixed(4);
-    	item.pos.y = item.pos.y.fixed(4);
-}, 
-
 process_input: function( player ) 
 {
 	//It's possible to have recieved multiple inputs by now,
--- client.player.js
+++ client.player.js
@@ -29,14 +29,6 @@ initialize: function( game_instance, player_instance )
             //Our local history of inputs
         this.inputs = [];
 
-            //The world bounds we are confined to
-        this.pos_limits = {
-            x_min: this.size.hx,
-            x_max: this.game.world.width - this.size.hx,
-            y_min: this.size.hy,
-            y_max: this.game.world.height - this.size.hy
-        };
-
             //The 'host' of a game gets created with a player instance since
             //the server already knows who they are. If the server starts a game
             //with only a host, the other player is set up in the 'else' below
