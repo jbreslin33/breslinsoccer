db21fdcf21553f1f811454768fde3425f290c681
Jim Breslin <jbreslin33@gmail.com>
2017-07-08 17:27:02 -0400

    af

---
+++ client.world.js
@@ -0,0 +1,15 @@
+/*
+    The world class
+
+        A simple class to set the out limits of where an object can go.
+*/
+var ClientWorld = new Class(
+{
+initialize: function(width,height) 
+{
+        this.width = width;
+        this.height = height;
+    } 
+});
+    
+module.exports = ClientWorld;
