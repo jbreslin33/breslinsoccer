e706e37ab325e62be5a9b02eaca2e3a9abf1aabe
Jim Breslin <jbreslin33@gmail.com>
2017-08-10 18:55:25 -0400

    hosts

--- client.core.js
+++ client.core.js
@@ -537,12 +537,12 @@ client_create_debug_gui: function()
 
 client_reset_positions: function() 
 {
-	var player_host = this.clientPlayerArray[0].host ?  this.clientPlayerArray[0] : this.clientPlayerArray[1];
-    	var player_client = this.clientPlayerArray[0].host ?  this.clientPlayerArray[1] : this.clientPlayerArray[0];
+	//var player_host = this.clientPlayerArray[0].host ?  this.clientPlayerArray[0] : this.clientPlayerArray[1];
+    	//var player_client = this.clientPlayerArray[0].host ?  this.clientPlayerArray[1] : this.clientPlayerArray[0];
 
         //Host always spawns at the top left.
-    	player_host.pos = { x:20,y:20 };
-    	player_client.pos = { x:500, y:200 };
+    	this.clientPlayerArray[0].pos = { x:20,y:20 };
+    	this.clientPlyaerArray[1].pos = { x:500, y:200 };
 
         //Make sure the local player physics is updated
     	this.clientPlayerArray[0].old_state.pos = this.pos(this.clientPlayerArray[0].pos);
