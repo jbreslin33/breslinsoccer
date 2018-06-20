a6d91baec8b43caedc4f1a43a81b6c6eaf6c66c4
Jim Breslin <jbreslin33@gmail.com>
2018-06-19 21:58:27 -0400

    player script add gravity

---
+++ Player.gd
@@ -0,0 +1,41 @@
+extends KinematicBody
+
+var speed = 200
+var direction = Vector3()
+var gravity = -9.8
+var velocity = Vector3()
+
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func _physics_process(delta):
+	direction = Vector3(0,0,0)
+	if Input.is_action_pressed("ui_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		direction.x += 1
+	if Input.is_action_pressed("ui_up"):
+		direction.z -= 1
+	if Input.is_action_pressed("ui_down"):
+		direction.z += 1
+		
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	
+	
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
