49a0897ced91b34ae32a6c8a7bfea25c63c4b32c
Jim Breslin <jbreslin@gmail.com>
2018-07-01 15:57:01 -0400

    init

---
+++ ball/Ball.gd
@@ -0,0 +1,32 @@
+extends KinematicBody
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+var speed = 200;
+var direction = Vector3()
+var gravity = -9.8
+var velocity = Vector3()
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here.
+	pass
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
+
+func _physics_process(delta):
+	direction = Vector3(0,0,0)
+	
+	direction = direction.normalized()
+	direction = direction * speed * delta
+	
+	velocity.y += gravity * delta
+	velocity.x = direction.x
+	velocity.z = direction.z
+	
+	velocity = move_and_slide(velocity, Vector3(0,1,0))
+	
